4f898ffd3f1f4c79945bad33e41518e417269820
Christopher <kung_cool_yes@hotmail.com>
2016-03-22 14:48:27 +0100

    Har lagt till mer saker

---
+++ HBGDatorService/HBGDatorServiceDAL/HBGDatorServiceContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HBGDatorServiceDAL.POCO;
+namespace HBGDatorServiceDAL
+{
+    class HBGDatorServiceContext: DbContext 
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ HBGDatorService/HBGDatorServiceDAL/POCO/user.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HBGDatorServiceDAL
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int AdminLevel { get; set; }
+        public bool IsActive { get; set; }
+        public string Email { get; set; }
+       
+
+        public User()
+        {
+
+        }
+
+        public User(string username, string password, int adminLevel, string email)
+        {
+            Username = username;
+            Password = password;
+            AdminLevel = adminLevel;
+            Email = email;
+            IsActive = true;
+         
+        }
+    }
+}
