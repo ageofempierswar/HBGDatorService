1fdd0bfd503ab15cc61730a895e29d14d80ba6ee
Christopher <kung_cool_yes@hotmail.com>
2016-03-23 13:26:49 +0100

    la till en klass

---
+++ HBGDatorService/HBGDatorServiceDAL/Repository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+namespace HBGDatorServiceDAL
+{
+    class Repository
+    {
+    }
+}
