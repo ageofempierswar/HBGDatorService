119b6b6fbb4dd63ffff14ddea574c48a711fefe1
linusekdahl <linusekdahl@gmail.com>
2016-03-23 12:34:20 +0100

    Changed Projekt type to sort out a few problems

--- HBGDatorService/HBGDatorService/Controllers/HomeController.cs
+++ HBGDatorService/HBGDatorService/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace HBGDatorService.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            return View(); // 
         }
     }
 }
