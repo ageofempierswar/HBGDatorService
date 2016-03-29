de18a818fa6ee5c4e807e5ec58c9e50aabaa3f66
Christopher <kung_cool_yes@hotmail.com>
2016-03-29 11:20:31 +0200

    La till en ska i Contactini
    dropcreatedatabaseAlwase

--- HBGDatorService/HBGDatorServiceDAL/HBGDatorServiceContextInitializer.cs
+++ HBGDatorService/HBGDatorServiceDAL/HBGDatorServiceContextInitializer.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HBGDatorServiceDAL
 {
-    class HBGDatorServiceContextInitializer
+    class HBGDatorServiceContextInitializer : DropCreateDatabaseAlways<HBGDatorServiceContext>
     {
     }
 }
