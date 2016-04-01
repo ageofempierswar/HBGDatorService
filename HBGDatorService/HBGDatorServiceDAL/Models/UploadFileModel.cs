062f4173401b378d8481334df506fcb61b046e3c
Christopher <kung_cool_yes@hotmail.com>
2016-04-01 11:48:14 +0200

    Lakgt till upload funktioner

---
+++ HBGDatorService/HBGDatorServiceDAL/Models/UploadFileModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace HBGDatorServiceDAL.Models
+{
+    public class UploadFileModel
+    {
+        public int Id { get; set; }
+        [StringLength(255)]
+        public string FileName { get; set; }
+        [StringLength(100)]
+        public string ContentType { get; set; }
+        public byte[] Content { get; set; }
+        public FileType FileType { get; set; }
+    }
+
+    public enum FileType
+    {
+        Avatar = 1, Photo
+    }
+}
