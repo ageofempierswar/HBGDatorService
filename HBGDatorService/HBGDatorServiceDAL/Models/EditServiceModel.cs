ad84831d952cf27b42d7bcbd3d50f9fef74457c6
Christopher <kung_cool_yes@hotmail.com>
2016-04-14 10:53:29 +0200

    Fiaxat saker på Service

---
+++ HBGDatorService/HBGDatorServiceDAL/Models/EditServiceModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace HBGDatorServiceDAL.Models
+{
+    public class EditServiceModel
+    {
+        public int ID { get; set; }
+        public string Header1 { get; set; }
+        public string Header2 { get; set; }
+        public string Header3 { get; set; }
+        public string Header4 { get; set; }
+        public string Header5 { get; set; }
+        public string Header6 { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string Textfield1 { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string Textfield2 { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string Textfield3 { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string Textfield4 { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string Textfield5 { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string Textfield6 { get; set; }
+    }
+}
