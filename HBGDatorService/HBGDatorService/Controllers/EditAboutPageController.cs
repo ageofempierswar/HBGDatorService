7536ab9897c2c80607f5104eb9400780c861c1b4
Christopher <kung_cool_yes@hotmail.com>
2016-05-09 14:35:30 +0200

    FIXFIXIFX

--- HBGDatorService/HBGDatorService/Controllers/EditAboutPageController.cs
+++ HBGDatorService/HBGDatorService/Controllers/EditAboutPageController.cs
@@ -7,7 +7,8 @@ using System.Web.UI.WebControls.WebParts;
 using HBGDatorServiceDAL;
 using HBGDatorServiceDAL.Models;
 using HBGDatorServiceDAL.POCO;
-
+using System.Configuration;
+using System.Data.SqlClient;
 namespace HBGDatorService.Controllers
 {
     [Authorize]
@@ -30,6 +31,46 @@ namespace HBGDatorService.Controllers
             }
 
             return View(model);
+        }
+
+        public ActionResult AddEditAbout()
+        {
+            try
+            {
+                //Abouts abouts = new Abouts();
+                //About about    = new About(textBoxFirstName.Text, textBoxLastName.Text, textBoxEmail.Text, int.Parse(textBoxPersonID.Text));
+                //abouts.InsertPerson(about);
+            }
+            catch (Exception exception)
+            {
+           
+            }
+        }
+        public ActionResult UpdateEditAbout()
+        {
+            try
+            {
+                //Abouts abouts = new Abouts();
+                //About about    = new About(textBoxFirstName.Text, textBoxLastName.Text, textBoxEmail.Text, int.Parse(textBoxPersonID.Text));
+                //abouts.UpdatePerson(about);
+            }
+            catch (Exception exception)
+            {
+                
+            }
+        }
+        public ActionResult DeleteEditAbout()
+        {
+            try
+            {
+                //Abouts abouts = new Abouts();
+                //About about    = new About(textBoxFirstName.Text, textBoxLastName.Text, textBoxEmail.Text);
+
+            }
+            catch (Exception exception)
+            {
+            
+            }
         }
     }
 }
