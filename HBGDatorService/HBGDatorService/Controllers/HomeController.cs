be8c1cd207a5ed1b772cb47077798da903702d95
linusekdahl <linusekdahl@gmail.com>
2016-05-23 15:26:02 +0200

    commit

--- HBGDatorService/HBGDatorService/Controllers/HomeController.cs
+++ HBGDatorService/HBGDatorService/Controllers/HomeController.cs
@@ -11,16 +11,15 @@ namespace HBGDatorService.Controllers
 {
     public class HomeController : Controller
     {
+
+        private HBGDatorServiceContext db = new HBGDatorServiceContext();
+
         public ActionResult Index()
         {
             IndexPageModel ip = new IndexPageModel();
             var images = Repository.GetAllSlideShowImages();
             List<string> list = new List<string>();
-            list.Add("~/SlideImages/7cf0d7b2ceaf3358305aafa10b0f376c.jpg");
-            list.Add("~/SlideImages/all-mass-effect-characters.jpg");
-            list.Add("~/SlideImages/mass-effect-2-16685-1366x768.jpg");                       
-            list.Add("~/SlideImages/mass-effect-3-17004-1366x768.jpg");
-            list.Add("~/SlideImages/mass_effect_shepard_female_graphics_gun_1366x768_hd-wallpaper-15574.jpg");
+
             foreach (var SlideImage in images)
             {
                 list.Add(SlideImage.ImagePath);
@@ -71,12 +70,16 @@ namespace HBGDatorService.Controllers
         {
             var model = Repository.AboutReadOnly();
             return View(model);
+
+            //return View(db.Abouts.ToList());
         }
 
         public ActionResult Service()
         {
             var model = Repository.ServiceReadOnly(10);
             return View(model);
+
+            //return View(db.Service.ToList());
         }
     }
 }
